b1defc9c83dc3282498da116fe188f0ec086bebe
unknown <tannerbrunscheon@gmail.com>
2019-12-12 17:25:05 -0600

    Changed input types and added submit to form

--- src/SignupForm/SignupForm.js
+++ src/SignupForm/SignupForm.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
-export default function SignupForm({firstName, changeFirstName, email, changeEmail, password, changePassword}) {
+export default function SignupForm({firstName, changeFirstName, email, changeEmail, password, changePassword, onSubmit}) {
     return (
-        <form id="SignupForm">
+        <form id="SignupForm" onSubmit={onSubmit}>
             <header>
-                <p>Let's</p>
-                <b>Sign Up</b>
+                <h1>Let's</h1>
+                <h1><b>Sign Up</b></h1>
             </header>
             <p>Use the form below to sign up for this super awesome service. You're only a few steps away!</p>
             <label for="NameInput">First Name</label>
-            <input type="text" className="inputField" id="NameInput" value={firstName} onChange={(e)=> changeFirstName(e.target.value)}/>
+            <input type="text" className="inputField" id="NameInput" value={firstName} required onChange={(e)=> changeFirstName(e.target.value)}/>
             <label for="EmailInput">Email Address</label>
-            <input type="text" className="inputField" id="EmailInput" value={email}  onChange={(e)=> changeEmail(e.target.value)}/>
+            <input type="email" className="inputField" id="EmailInput" value={email} required onChange={(e)=> changeEmail(e.target.value)}/>
             <label for="PasswordInput">Password</label>
-            <input type="text" className="inputField" id="PasswordInput" value={password}  onChange={(e)=> changePassword(e.target.value)}/>
+            <input type="password" className="inputField" id="PasswordInput" value={password} required onChange={(e)=> changePassword(e.target.value)}/>
+            <button type="submit">Sign Up</button>
         </form>
     )
 }
